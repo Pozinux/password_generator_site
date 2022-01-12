cde941b240b10273d365a2b880a661148d99ee25
Pozinux <tim.poznanski@gmail.com>
2022-01-12 18:03:04 +0100

    Update

--- password_generator_src/password_generator_app/views.py
+++ password_generator_src/password_generator_app/views.py
@@ -18,7 +18,7 @@ def password_function(request):
     if request.GET.get('numbers'):
         characters.extend(list('0123456789'))
 
-    length = int(request.GET.get('lenght', 12))
+    length = int(request.GET.get('lenght', 15))
 
     the_password = ''
     for x in range(length):
