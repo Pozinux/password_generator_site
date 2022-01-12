1324749e599617bc0a998ec7337baf74520b6ddc
Pozinux <tim.poznanski@gmail.com>
2022-01-12 22:04:19 +0100

    Disable DEBUG

--- password_generator_src/password_generator_project/settings.py
+++ password_generator_src/password_generator_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'XXXX'  # Voir le fichier settings.py
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['tpoz.fr', 'www.tpoz.fr']
 
