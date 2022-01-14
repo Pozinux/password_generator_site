5a3e56b302e9f0756ccb28f2485169dcca80ea62
Pozinux <tim.poznanski@gmail.com>
2022-01-14 23:36:11 +0100

    Plus en ligne donc retrait du allowed_hosts

--- password_generator_src/password_generator_project/settings.py
+++ password_generator_src/password_generator_project/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['tpoz.fr', 'www.tpoz.fr']
+ALLOWED_HOSTS = []
 
 
 # Application definition
